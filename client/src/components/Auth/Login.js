9de315afeab3c78b1dc3cc8e6819e5f590f0fb75
mrsalvini <dsgmcosta@gmail.com>
2019-08-27 12:26:08 +0100

    session persists

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,31 +1,56 @@
-import React from "react"
-import { GraphQLClient } from 'graphql-request'
-import { GoogleLogin } from 'react-google-login'
+import React, { useContext } from "react"
+import { GraphQLClient } from "graphql-request"
+import { GoogleLogin } from "react-google-login"
 import { withStyles } from "@material-ui/core/styles"
-// import Typography from "@material-ui/core/Typography"
+import Typography from "@material-ui/core/Typography"
 
-const ME_QUERY = `
-  {
-    me {
-      _id
-      name
-      email
-      picture
-    }
-  }
-`
+import Context from "../../context"
+import { ME_QUERY } from "../../graphql/queries"
+import { BASE_URL } from "../../client"
 
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context)
+
   const onSuccess = async googleUser => {
-    const idToken = googleUser.getAuthResponse().id_token
-    const client = new GraphQLClient('http://localhost:4000/graphql', {
-      headers: { authorization: idToken }
-    })
-    const data = await client.request(ME_QUERY)
-    console.log({ data })
+    try {
+      const idToken = googleUser.getAuthResponse().id_token
+      const client = new GraphQLClient(BASE_URL, {
+        headers: { authorization: idToken }
+      })
+      const { me } = await client.request(ME_QUERY)
+      dispatch({ type: "LOGIN_USER", payload: me })
+      dispatch({ type: "IS_LOGGED_IN", payload: googleUser.isSignedIn() })
+    } catch (err) {
+      onFailure(err)
+    }
   }
 
-  return <GoogleLogin clientId="205613349103-vf16g2g17q34bs89ocq9ui7p461bbuab.apps.googleusercontent.com" onSuccess={onSuccess} signedIn={true} />
+  const onFailure = err => {
+    console.error("Error logging in", err)
+    dispatch({ type: "IS_LOGGED_IN", payload: false })
+  }
+
+  return (
+    <div className={classes.root}>
+      <Typography
+        component="h1"
+        variant="h3"
+        gutterBottom
+        noWrap
+        style={{ color: "rgb(66, 133, 244)" }}
+      >
+        Welcome
+      </Typography>
+      <GoogleLogin
+        clientId="205613349103-vf16g2g17q34bs89ocq9ui7p461bbuab.apps.googleusercontent.com"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        isSignedIn={true}
+        buttonText="Login with Google"
+        theme="dark"
+      />
+    </div>
+  )
 }
 
 const styles = {
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,8 +1,17 @@
-import React from "react";
-import withRoot from "../withRoot";
+import React from "react"
+
+import Header from "../components/Header"
+import Map from '../components/Map'
+import withRoot from "../withRoot"
+
 
 const App = () => {
-  return <div>App</div>;
-};
+  return (
+    <>
+      <Header />
+      <Map />
+    </>
+  )
+}
 
-export default withRoot(App);
+export default withRoot(App)
